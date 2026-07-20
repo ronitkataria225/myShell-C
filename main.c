d42841d8064d817b30f2e5bb1ad9fb417c3aac02
ronitkataria225 <ronitkataria225@gmail.com>
2026-07-20 10:34:02 +0530

    Implement REPL

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    
+  setbuf(stdout, NULL); // Flush after every printf
+  char input[100];
+
+  while (1) {
+    printf("$ ");
+    scanf("%s", input);
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
